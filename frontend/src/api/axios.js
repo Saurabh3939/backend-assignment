c84fd3dae28915983f52a2a0257cdf6f65ab24d0
Saurabh Lendave <saurabhlendave3939@gmail.com>
2026-02-06 21:45:49 +0530

    Fix: Backend URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://backend-assignment-yr8c.onrender.com",
+  baseURL: "https://backend-assignment-yr8c.onrender.com/api/v1",
   withCredentials: true,
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
-    req.headers.Authorization = ` Bearer ${token}`;
+    req.headers.Authorization = `Bearer ${token}`;
   }
   return req;
 });
